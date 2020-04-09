2e4e887ea67e866a02427d67fa40af37e7479d60
bayuwibawa15 <wibawabayu015@gmail.com>
2020-04-10 01:19:38 +0700

    Percabangan Switch

---
+++ src/percabangan/Switch.java
@@ -0,0 +1,24 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int nama=1;
+        switch (nama){
+            case 1:
+                System.out.println("bayuwibawa");
+                break;
+            case 2:
+                System.out.println("fauzan");
+                break;
+            case 3:
+                System.out.println("ancahlajuardi");
+                break;
+            case 4:
+                System.out.println("claudia");
+                break;
+            case 5:
+                System.out.println("mahdun");
+                break;
+        }
+    }
+}
