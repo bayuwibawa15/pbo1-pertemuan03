dfa313394ec13a1e986bd01f620fcd580812ede7
bayuwibawa15 <wibawabayu015@gmail.com>
2020-04-10 00:44:30 +0700

    Operator Binary Aritmatika

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,16 @@
+package operator;
+
+public class OperatorBinary {
+    String nama = "Bayu Wibawa";
+    int jumlahSks =24;
+    double indeksPrestasiKumulatif = 3.89;
+
+    public static void main(String[] args) {
+        System.out.println(15+2);
+        System.out.println(15-2);
+        System.out.println(15*2);
+        System.out.println(15/2);
+        System.out.println(15%2);
+        System.out.println(15%3);
+    }
+}
