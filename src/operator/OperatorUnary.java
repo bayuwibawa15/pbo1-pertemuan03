ac74752527ff139f078378885b2c86075fcb905f
bayuwibawa15 <wibawabayu015@gmail.com>
2020-04-10 00:50:17 +0700

    OperatorUnary

---
+++ src/operator/OperatorUnary.java
@@ -0,0 +1,18 @@
+package operator;
+
+public class OperatorUnary {
+    public static void main(String[] args) {
+        int i = 15;
+        i++;
+        System.out.println(i);
+        i++;
+        System.out.println(i);
+        i=i+3;
+        System.out.println(i);
+        i+=4;
+        System.out.println(i);
+        i*=1;
+        System.out.println(i);
+
+    }
+}
