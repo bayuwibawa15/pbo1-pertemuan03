ea4c5d7472c4cff8049750ccc1ea57abccba9c81
bayuwibawa15 <wibawabayu015@gmail.com>
2020-04-10 01:14:45 +0700

    If Else Statement 3

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -3,10 +3,13 @@ package percabangan;
 public class IfElse {
     public static void main(String[] args) {
         double totalBelanja = 45000;
-        double uangDiSaku = 86000;
+        double uangDiSaku = 78000;
 
-        if (uangDiSaku<totalBelanja){
+        if (uangDiSaku<totalBelanja) {
             System.out.println("Uang kurang , kurangi belanjaan anda");
+        }else if(uangDiSaku>totalBelanja){
+            double angsul = uangDiSaku-totalBelanja;
+            System.out.println("Angsulan anda "+angsul);
         }else{
             System.out.println("Uang cukup, selamat menikmati");
         }
